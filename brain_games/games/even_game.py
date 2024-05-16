e60aa51428a32499ad5a8bbcbd9888d48e36ad77
kozlyatinka <kozlyatinka@bk.ru>
2024-05-16 17:05:44 +0300

    edit file

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -5,30 +5,30 @@ import random
 import prompt
 
 
-def even_game():
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+def even_game(game):
     name = prompt.string('May I have your name? ')
     answers_count = 0
     print(f"Hello, {name}!")
-    print("Answer 'yes' if the number is even, otherwise answer 'no'.")
 
     for i in range(1, 4):
         number = random.randint(0, 100)
         print(f"Question: {number}")
-        answer = prompt.string('Your answer: ')
-        if number % 2 == 0 and answer.lower() == "yes":
+        answer = prompt.string('Your answer: ').lower()
+        if (number % 2 == 0 and answer == "yes") or (number % 2 != 0 and answer == "no"):
             print("Correct!")
             answers_count += 1
             continue
-        elif number % 2 == 0 and answer.lower() == "no":
-            print(f"'no' is wrong answer ;(. Correct answer was 'yes'.\n Let's try again, {name}!")
-            continue
-        elif number % 2 != 0 and answer.lower() == "no":
-            print("Correct!")
-            answers_count += 1
-            continue
-        elif number % 2 != 0 and answer.lower() == "yes":
-            print("'yes' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, {name}!")
+        elif (number % 2 == 0 and answer == "no") or (number % 2 != 0 and answer == "yes"):
+            if answer == "no":
+                correct_answer = "yes"
+            if answer == "yes":
+                correct_answer = "no"
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
             break
+            
         else:
             print("Incorrect input")
 
