96c34efc73e1077f92cd0bf61863d14ca6ad34dc
kozlyatinka <kozlyatinka@bk.ru>
2024-05-17 12:58:25 +0300

    edit files

--- brain_games/welcome.py
+++ brain_games/welcome.py
@@ -4,6 +4,5 @@ import prompt
 def welcome_user(game):
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
-    return name
     print(f'Hello, {name}!')
     print(game.DESCRIPTION)
