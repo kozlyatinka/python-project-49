86e7a5ed39e87622c35a24aecd984a30c1af8f73
kozlyatinka <kozlyatinka@bk.ru>
2024-05-20 13:43:37 +0300

    edit prime-game

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 import random
 
-DESCRIPTION = 'Is given number is prime?'
+DESCRIPTION = 'Answer "yes" if the number is is prime, otherwise answer "no".'
 
 
 def func_game():
     random_number = random.randint(1, 100)
     question = random_number
     if random_number > 1:
-        for i in range(2. (random_number // 2) + 1):
+        for i in range(2, (random_number // 2) + 1):
             if random_number % i == 0:
                 correct_answer = 'no'
-                break
             else:
                 correct_answer = 'yes'
     else:
