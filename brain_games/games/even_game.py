a31f571f935fbbd88d6f30b1cd2e3b0d1bf9f1da
kozlyatinka <kozlyatinka@bk.ru>
2024-05-28 14:48:52 +0300

    edit even file

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 import random
 
-
+LOWER_LIMIT = 1
+UPPER_LIMIT = 200
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def func_game():
-    random_number = random.randint(1, 100)
+    random_number = random.randint(LOWER_LIMIT, UPPER_LIMIT)
     question = random_number
-    if random_number % 2 == 0:
+    if even(random_number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
     return correct_answer, question
+
+def even(random_number):
+    return random_number % 2 == 0
