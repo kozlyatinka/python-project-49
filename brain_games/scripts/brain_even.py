9a782adbb98ee2c2c819d637c345448e1cf60e11
kozlyatinka <kozlyatinka@bk.ru>
2024-05-28 16:04:21 +0300

    edit brain even file

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.welcome import welcome_user
+from brain_games.engine import launch
 from brain_games.games import even_game
 
 
 def main():
-    welcome_user(even_game)
+    launch(even_game)
 
 
 if __name__ == '__main__':
