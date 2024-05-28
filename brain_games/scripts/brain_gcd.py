7acbcf92a4cff6376ee8138b04e60d6416d7f20d
kozlyatinka <kozlyatinka@bk.ru>
2024-05-28 16:06:36 +0300

    edit brain gcd file

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.welcome import welcome_user
+from brain_games.engine import launch
 from brain_games.games import gcd_game
 
 
 def main():
-    welcome_user(gcd_game)
+    launch(gcd_game)
 
 
 if __name__ == '__main__':
