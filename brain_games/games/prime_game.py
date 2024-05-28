884b19a17d50a743c8adacc9fd32b50728fa9134
kozlyatinka <kozlyatinka@bk.ru>
2024-05-28 16:19:12 +0300

    edit prime file

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -18,8 +18,8 @@ def get_values():
 
 def prime(random_number):
     if random_number > 1:
-        for i in range(2, int(random_number ** 0.5) + 1):
-            if random_number % i == 0:
+        for i in range(2, random_number):
+            if not (random_number % i) == 0:
                 return False
             else:
                 return True
