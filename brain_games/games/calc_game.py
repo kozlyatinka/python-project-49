92fbc069add8d2d5b110e9b97d3bd0f1a8c7d951
kozlyatinka <kozlyatinka@bk.ru>
2024-05-28 16:09:57 +0300

    edit calc game file

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 import random
-
+LOWER_LIMIT = 1
+UPPER_LIMIT = 200
 
 DESCRIPTION = 'What is the result of the expression?'
 
 
-def func_game():
-    random_one = random.randint(1, 100)
-    random_two = random.randint(1, 100)
+def get_values():
+    random_one = random.randint(LOWER_LIMIT, UPPER_LIMIT)
+    random_two = random.randint(LOWER_LIMIT, UPPER_LIMIT)
     random_operator = random.choice('+-*')
     question = f'{random_one} {random_operator} {random_two}'
     correct_answer = str(calculate(random_one, random_two, random_operator))
