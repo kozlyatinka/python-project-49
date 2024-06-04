a6ca2de90dc5938a9cfa4e4ea5dae214b017f755
kozlyatinka <kozlyatinka@bk.ru>
2024-06-04 15:16:28 +0300

    edit prime file

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -9,18 +9,16 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def get_values():
     random_number = random.randint(LOWER_LIMIT, UPPER_LIMIT)
     question = random_number
-    if not prime(random_number):
+    if not is_prime(random_number):
         correct_answer = 'no'
     else:
         correct_answer = 'yes'
     return correct_answer, question
 
 
-def prime(random_number):
+def is_prime(random_number):
     if random_number < 1 or random_number % 2 == 0:
         return False
-    if random_number == 2:
-        return True
     for i in range(3, int(random_number ** 0.5) + 1, 2):
         if random_number % i == 0:
             return False
