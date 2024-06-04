4b143f792acb947c7dde8ac33290d030d6ba85a0
kozlyatinka <kozlyatinka@bk.ru>
2024-06-04 15:06:54 +0300

    edit make-progression file

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -4,11 +4,14 @@ import random
 LOWER_LIMIT = 1
 STEP_UPPER_LIMIT = 4
 UPPER_LIMIT = 20
+LENGTH = 10
 DESCRIPTION = 'What number is missing in the progression?'
 
 
 def get_values():
-    progression = get_progression(LOWER_LIMIT, UPPER_LIMIT, STEP_UPPER_LIMIT)
+    initial_term = random.randint(LOWER_LIMIT, UPPER_LIMIT)
+    common_difference = random.randint(LOWER_LIMIT, STEP_UPPER_LIMIT)
+    progression = get_progression(initial_term, common_difference, LENGTH)
     random_answer_index = random.randint(LOWER_LIMIT, len(progression) - 1)
     correct_answer = str(progression[random_answer_index])
     progression[random_answer_index] = ".."
@@ -16,9 +19,10 @@ def get_values():
     return correct_answer, question
 
 
-def get_progression(LOWER_LIMIT, UPPER_LIMIT, STEP_UPPER_LIMIT):
+def get_progression(initial_term, common_difference, LENGTH):
     number_list = []
     random_step = random.randint(LOWER_LIMIT, STEP_UPPER_LIMIT)
-    for number in range(LOWER_LIMIT, UPPER_LIMIT, random_step):
+    end_progression = initial_term + (LENGTH * random_step)
+    for number in range(initial_term, end_progression, common_difference):
         number_list.append(number)
     return number_list
